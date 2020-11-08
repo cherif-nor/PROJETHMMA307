577b6211bec1148ef9453e032d32d8fc25532fb9
mohamedcherif <amgharchrif@gmail.com>
2020-11-08 21:14:11 +0100

    update code.py

--- code.py
+++ code.py
@@ -43,11 +43,11 @@ plt.show()
 #bootstrap parametrique:
 ####################################
 # configure bootstrap
-n_iterations = 1000
+nboot= 1000
 n_size = int(len(Estuaries) * 0.50)
 # run bootstrap
 stats = list()
-for i in range(n_iterations):
+for i in range(nboot):
 	# prepare train and test sets
 	train = resample(values, n_samples=n_size)
 	test = numpy.array([x for x in values if x.tolist() not in train.tolist()])
@@ -77,5 +77,40 @@ md=smf.mixedlm("Total ~ Modification", Estuaries, groups= Estuaries["Estuary","S
 res=md.fit().summary()
 print(res)
 
+#### anova pour tester les effets aléatoires
+ = sm.datasets.longley.load(as_pandas=False)
+>>> exog = sm.add_constant(data.exog)
+>>> mod_fit = sm.ols(data.endog, exog).fit()
+>>> res = mod_fit.resid # residuals
+>>> fig = sm.qqplot(res)
+>>> plt.show()
 
+### bootstrap paramétrique: pour tester les effets aleatoires.
+
+nboot= 1000
+n_size = int(len(Estuaries) * 0.60)
+# run bootstrap
+stats = list()
+for i in range(nboot):
+	# prepare train and test sets
+	train = resample(values, n_samples=n_size)
+	test = numpy.array([x for x in values if x.tolist() not in train.tolist()])
+	# fit model
+	model = DecisionTreeClassifier()
+	model.fit(train[:,:-1], train[:,-1])
+	# evaluate model
+	predictions = model.predict(test[:,:-1])
+	score = accuracy_score(test[:,-1], predictions)
+	print(score)
+	stats.append(score)
+# plot scores
+pyplot.hist(stats)
+pyplot.show()
+# confidence intervals
+alpha = 0.95
+p = ((1.0-alpha)/2.0) * 100
+lower = max(0.0, numpy.percentile(stats, p))
+p = (alpha+((1.0-alpha)/2.0)) * 100
+upper = min(1.0, numpy.percentile(stats, p))
+print('%.1f confidence interval %.1f%% and %.1f%%' % (alpha*100, lower*100, upper*100))
 
